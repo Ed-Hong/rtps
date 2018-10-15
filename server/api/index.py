a0710b4006ec9d381f246973560d2f70bebe547e
Edward Hong <edward.xc.hong@gmail.com>
2018-10-14 22:27:29 -0500

    begin basic GET and POST endpoints for spot statuses

--- server/api/index.py
+++ server/api/index.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
+mock = [
+  { 'lotId': 1, 'locationId': 1, 'spotId': 1, 'isFull': True }
+]
 
-@app.route("/")
-def hello_world():
-  return "Hello, World!"
+# GET API status
+@app.route('/')
+def api_status():
+  return 'RTPS API is Alive \n'
+
+# GET endpoint for spot statuses
+@app.route('/spots')
+def get_spots():
+  return jsonify(mock)
+
+# POST endpoint for updating spot statuses
+@app.route('/spots', methods=['POST'])
+def add_spot():
+  mock.append(request.get_json())
+  return '', 204
